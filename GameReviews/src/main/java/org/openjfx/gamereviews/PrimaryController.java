86acd11e7d5423b0f8acb272e621a8261825246d
NicklasKleemann <71381595+NicklasKleemann@users.noreply.github.com>
2021-11-12 09:54:22 +0100

    wasd

--- GameReviews/src/main/java/org/openjfx/gamereviews/PrimaryController.java
+++ GameReviews/src/main/java/org/openjfx/gamereviews/PrimaryController.java
@@ -1,12 +1,22 @@
 package org.openjfx.gamereviews;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
+       int n = 100;
+
+    System.out.println( RandomString.getAlphaNumericString(n));
+      
+
         App.setRoot("secondary");
     }
 }
+
+
+
+
