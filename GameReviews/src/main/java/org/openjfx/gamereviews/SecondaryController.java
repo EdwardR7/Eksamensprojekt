83f19baf43dd0e762041b77971213f68f4028e9a
NicklasKleemann <71381595+NicklasKleemann@users.noreply.github.com>
2021-12-05 21:19:08 +0100

    wasd

--- GameReviews/src/main/java/org/openjfx/gamereviews/SecondaryController.java
+++ GameReviews/src/main/java/org/openjfx/gamereviews/SecondaryController.java
@@ -63,6 +63,7 @@ public class SecondaryController {
                     int temp = charArray[i];
                     charArray[i] = charArray[j];
                     charArray[j] = (char) temp;
+                    
                 }
             }
         }
